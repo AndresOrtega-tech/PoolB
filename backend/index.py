641242e0e6a0699e8cba83e990a0d68b39857b4d
AndresOrtega-tech <tamezandres191@gmail.com>
2025-08-04 12:46:02 -0600

    Se configura 'vercel.json'de manera correcta y se agrega 'index.py'

--- backend/index.py
+++ backend/index.py
@@ -1,8 +1,4 @@
 from main import app
 
-# Vercel necesita esta estructura
-def handler(event, context):
-    return app
-
-# También exportamos la app directamente
-application = app
+# Exportar la app para Vercel
+app = app
